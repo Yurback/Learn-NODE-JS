5942bd5bdf1bc40ee1c900431e9aad7c71541160
Yurback <ryv1983@mail.ru>
2020-03-10 01:27:48 +0300

    добавил ссылку на favicon

--- HOMEWORK_2/app.js
+++ HOMEWORK_2/app.js
@@ -4,26 +4,27 @@ const app = express();
 
 
 // 1 part
-// app.use((req,res,next)=>{
-//     console.log("1");
-//     next();
-// });
+app.use((req,res,next)=>{
+    console.log("1");
+    next();
+});
 
-// app.use((req,res,next)=>{
-//     console.log("2"); 
-//     res.send('<h1>This is my first part</h1>')
-// });
+app.use((req,res,next)=>{
+    console.log("2"); 
+    res.send('<link rel="shortcut icon" href="http://localhost/" />' +
+    '<h1>This is my first part</h1>');
+});
 
 // 2 part
-app.use('/users', (req,res,next)=>{
-    console.log("1");
-    res.send('<h1>Hello user!</h1>');
-});
+// app.use('/users', (req,res,next)=>{
+//     console.log('1');
+//     res.send('<head><link rel="shortcut icon" href="http://localhost/" /></head><p>Hello user!</p>');
+// });
 
-app.use('/', (req,res,next)=>{
-    console.log("2"); 
-    res.send('<h1>This is dummy</h1>');
-});
+// app.use('/', (req,res,next)=>{
+//     console.log('2'); 
+//     res.send('<head><link rel="shortcut icon" href="http://localhost/" /></head><p>This is dummy</p>');
+// });
 
 app.listen(3000);
 
